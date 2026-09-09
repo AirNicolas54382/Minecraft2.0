c8458e80b22d733fdd7b9a552ea1f997a6b96394
Mikolaj <108267340+AirNicolas54382@users.noreply.github.com.>
2026-09-09 14:21:31 +0200

    feat: add main execution class

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+
+import Frames.Player_choose;
+import Mechanics.Boss_fight;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+       /* try {
+            Boss_fight boss = new Boss_fight();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+         Player_choose player = new Player_choose();
+
+    }
+
+}
